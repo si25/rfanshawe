27faa556e91a6250af7ac9ffc20592b82f184baf
si25 <simon-gibbs@hotmail.com>
2020-03-30 10:35:10 +0100

    First commit

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+//$(function() {
+//    $('.carousel').each(function(){
+//        $(this).carousel({
+//            interval: false
+//        });
+//    });
+//});
+//
+////$(document).ready( function() {
+////    var divH = $(".carousel-inner").height();
+////    console.log( divH );
+////});
+////
+////$(document).ready( function() {
+////    var divH = $(".carousel-inner").width();
+////    console.log( divH );
+////});
